72f08e30eb473d451148b97260f319e9d68787fa
poongodi kandhsamy <poongodi.kandhsamy@3analytics.com>
2025-06-16 14:59:11 +0530

    commit Entrollment model

--- collegeapp/models/Course.py
+++ collegeapp/models/Course.py
@@ -1,10 +1,8 @@
 from django.db import models
 from safedelete import SOFT_DELETE
 from safedelete.models import SafeDeleteModel
+from collegeapp.models.Instructor import Instructor
 
-
-class InstructorProfile:
-    pass
 
 
 class Course(SafeDeleteModel):
@@ -13,4 +11,10 @@ class Course(SafeDeleteModel):
     title = models.CharField(max_length=100)
     subject=models.CharField(max_length=100)
     duration=models.CharField(max_length=50)
-    instructor=models.ForeignKey(InstructorProfile,on_delete=models.CASCADE,related_name='courses' )
+    instructor=models.ForeignKey(Instructor,on_delete=models.CASCADE,related_name='courses' )
+
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
